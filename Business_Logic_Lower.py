3e7c8803bbd8da754130f50ef262fb755609cb48
Ivan K <kosi17100@gmail.com>
2025-01-05 16:49:14 +0300

    (main:BLL): Эдобавлена функция отслеживания созданных каталогов

--- Business_Logic_Lower.py
+++ Business_Logic_Lower.py
@@ -1,6 +1,16 @@
 import os
+from Business_Logic_Upper import *
 
 
 # функция проверки существования каталога перед созданием каталога
 def exist_catalog():
-    return os.path.isfile(".txt")
+    path = os.getcwd()
+    for file in os.listdir(path):
+        if file.endswith(".txt"):
+            return True
+    return False
+
+# функция ведения реестра созданных каталогов
+list_catalog = []
+def reestr_catalog(name_catalog):
+    list_catalog.append(name_catalog)
