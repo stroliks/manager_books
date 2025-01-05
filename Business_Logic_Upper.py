92db107e9fde84bce3202855173a56b32ef294da
Ivan K <kosi17100@gmail.com>
2025-01-05 13:20:47 +0300

    (main: BLU): функция учета созданных каталогов выделана в отдельную функцию

--- Business_Logic_Upper.py
+++ Business_Logic_Upper.py
@@ -2,19 +2,20 @@
 import GraficUserInterface
 from Bussiness_Logic_Lower import *
 
-
+###name_catalog = message_create_catalog()
 
 # ФУНКЦИИ ПРИ СОЗДАНИИ КАТАЛОГА
 #___________________________________________________________________________________________________________________
 # функция создания каталога и присвоения ему имени
-def create_catalog():
-    if exist_catalog() == True:
-        return False
-    list_catalog = []
-    name_catalog = message_create_catalog()
+def create_catalog(name_catalog):
     catalog = open(name_catalog, "w")
-    list_catalog.append(name_catalog)
     return True
+
+def list_catalog(name_catalog):
+    list_catalog.append(name_catalog)
+    return list_catalog
+
+
 
 # ФУНКЦИИ ПРИ УДАЛЕНИИ КАТАЛОГА
 #____________________________________________________________________________________________________________________
