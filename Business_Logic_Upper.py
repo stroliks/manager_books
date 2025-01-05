7a98194c5da05048a588b356ea795ecd106234d3
Ivan K <kosi17100@gmail.com>
2025-01-05 22:29:18 +0300

    (main: BLU): Создана функция работы с каталогом, в нее ввязаны функции добавления и отображения каталога

--- Business_Logic_Upper.py
+++ Business_Logic_Upper.py
@@ -2,9 +2,23 @@ from Business_Logic_Lower import *
 
 
 
+def work_with_catalog(file):
+        from GraficUserInterface import msg_select_act_in_catalog, message_add_book
+        action_in_catalog = msg_select_act_in_catalog()
+        while action_in_catalog != "Н":
+            if action_in_catalog == "Д":
+                name_book, author_book, year_book, genre_book = message_add_book()
+                add_book(file, name_book, author_book, year_book, genre_book)
+pass
+            #elif action_in_catalog == "У":
+            #elif action_in_catalog == "О":
+            #elif action_in_catalog == "П":
+            #elif action_in_catalog == "С":
+            #elif action_in_catalog == "Э":
 
 
 # ФУНКЦИЯ НЕПОСРЕДСТВЕННОГО ФУНКЦИОНИРОВАНИЯ ПРОГРАММЫ ПРИ ВЫБОРЕ ПОЛЬЗОВАТЕЛЯ
+
 def global_function(action):
     from GraficUserInterface import message_first_select
     if action == "С":
@@ -32,18 +46,18 @@ def global_function(action):
         from GraficUserInterface import message_catalogs_history
         message_catalogs_history(list_catalog)
 
-    #elif:
-    #return
-        #action == "Р":
-        #action_in_catalog = msg_select_act_in_catalog()
-        #while action_in_catalog != "Н":
-            #if action_in_catalog == "Д":
-            #elif action_in_catalog == "У":
-            #elif action_in_catalog == "О":
-            #elif action_in_catalog == "П":
-            #elif action_in_catalog == "С":
-            #elif action_in_catalog == "Э":
-    #pass
+    elif action == "Р":
+        result_exist = exist_catalog()
+        if result_exist == True:
+            path = os.getcwd()
+            work_file = None
+            for work_file in os.listdir(path):
+                if work_file.endswith(".txt"):
+                    return work_file
+            work_with_catalog(work_file)
+        else:
+            print("КАталог книг отсутствует. Для начала создайте каталог!")
+
 
 
 
@@ -85,6 +99,7 @@ def view_catalog(file):
     catalog = file.readlines()
     for line in catalog:
         print(line)
+        print()
 
 # функция поиска книги в каталоге
 
