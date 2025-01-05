fc6de4905fd54e264e9dbb8095a649a21424db32
Ivan K <kosi17100@gmail.com>
2025-01-05 13:00:56 +0300

    (main: BLU): В функция создания каталога добавлена функция добавления создаваемых каталогов в список
    Для дальнейшей реализации функции вывода истории созданий каталогов

--- Business_Logic_Upper.py
+++ Business_Logic_Upper.py
@@ -3,10 +3,18 @@ import GraficUserInterface
 from Bussiness_Logic_Lower import *
 
 
+
+# ФУНКЦИИ ПРИ СОЗДАНИИ КАТАЛОГА
+#___________________________________________________________________________________________________________________
 # функция создания каталога и присвоения ему имени
 def create_catalog():
     if exist_catalog() == True:
         return False
-    catalog = open(message_create_catalog(), "w")
+    list_catalog = []
+    name_catalog = message_create_catalog()
+    catalog = open(name_catalog, "w")
+    list_catalog.append(name_catalog)
     return True
 
+# ФУНКЦИИ ПРИ УДАЛЕНИИ КАТАЛОГА
+#____________________________________________________________________________________________________________________
