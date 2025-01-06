19dbfbf9e4997ca115044d59f9e07b023e993e05
Ivan K <kosi17100@gmail.com>
2025-01-06 14:49:27 +0300

    (main:BLU): Устранена ошибка при работе функции wwc (удален лишний return в Р, вставлена функция запроса действия для повторения цикла

--- Business_Logic_Upper.py
+++ Business_Logic_Upper.py
@@ -1,49 +1,9 @@
 from Business_Logic_Lower import *
-import pandas
+import csv
 
 
-def work_with_catalog(file):
-        from GraficUserInterface import msg_select_act_in_catalog, message_add_book
-        action_in_catalog = msg_select_act_in_catalog()
-        while action_in_catalog != "Н":
 
-            if action_in_catalog == "Д":
-                name_book, author_book, year_book, genre_book = message_add_book()
-                add_book(file, name_book, author_book, year_book, genre_book)
-
-            elif action_in_catalog == "У":
-                from GraficUserInterface import message_delete_book, view_list_books
-                view_list_books(file)
-                number_book = message_delete_book()
-                delete_book(file, number_book)
-
-            elif action_in_catalog == "О":
-                from GraficUserInterface import view_list_books
-                print(f"В текущем каталоге находятся следующие книги:  ")
-                view_list_books(file)
-
-            elif action_in_catalog == "П":
-                from GraficUserInterface import message_search_books, print_list
-                searсh_word = message_search_books()
-                catalog = search_book(file, searсh_word)
-                print_list(catalog)
-
-            elif action_in_catalog == "С":
-                from GraficUserInterface import message_sort_books, print_list
-                sort_atribute = message_sort_books()
-                sort_books = sort_catalog(file, sort_atribute)
-                print_list(sort_books)
-
-            elif action_in_catalog == "Э":
-                from GraficUserInterface import message_export_catalog
-                format = message_export_catalog()
-                if format == "C":
-                    export_catalog(file, name_catalog)
-                    return True
-                return False
-
-
-# ФУНКЦИЯ НЕПОСРЕДСТВЕННОГО ФУНКЦИОНИРОВАНИЯ ПРОГРАММЫ ПРИ ВЫБОРЕ ПОЛЬЗОВАТЕЛЯ
+# ФУНКЦИЯ НЕПОСРЕДСТВЕННОГО ФУНКЦИОНИРОВАНИЯ ПРОГРАММЫ ПРИ ПЕРВОМ ВЫБОРЕ ПОЛЬЗОВАТЕЛЯ
 
 def global_function(action):
     from GraficUserInterface import message_first_select
@@ -51,17 +11,18 @@ def global_function(action):
         from GraficUserInterface import message_create_catalog, message_result_create_catalog
         result_exist = exist_catalog()
         name_catalog = None
-        if result_exist == False:
+        if not result_exist:
             name_catalog = message_create_catalog()
             create_catalog(name_catalog)
             reestr_catalog(name_catalog)
         return message_result_create_catalog(result_exist, name_catalog)
 
     elif action == "У":
-        from GraficUserInterface import message_confirm_delete_catalog, message_result_delete_catalog, message_first_select, message_create_catalog
+        from GraficUserInterface import message_confirm_delete_catalog, message_result_delete_catalog, \
+            message_create_catalog
         if message_confirm_delete_catalog() == "Да":
             result_exist = exist_catalog()
-            if result_exist == True:
+            if result_exist:
                 delete_catalog()
             return message_result_delete_catalog(result_exist)
 
@@ -74,16 +35,62 @@ def global_function(action):
 
     elif action == "Р":
         result_exist = exist_catalog()
-        if result_exist == True:
+        if result_exist:
             path = os.getcwd()
-            work_file = None
+            path_file = None
             for work_file in os.listdir(path):
                 if work_file.endswith(".txt"):
-                    return work_file
-            work_with_catalog(work_file)
+                    path_file = os.path.abspath(work_file)
+            work_with_catalog(path_file)
         else:
-            print("КАталог книг отсутствует. Для начала создайте каталог!")
+            print("Каталог книг отсутствует. Для начала создайте каталог!")
+            print()
+            print()
 
+
+# ФУНКЦИЯ ФУНКЦИОНИРОВАНИЯ ПРОГРАММЫ ПРИ  РАБОТЕ C КАТАЛОГОМ
+
+def work_with_catalog(path_file):
+    from GraficUserInterface import msg_select_act_in_catalog
+    action_in_catalog = msg_select_act_in_catalog()
+    while action_in_catalog != "Н":
+
+        if action_in_catalog == "Д":
+            from GraficUserInterface import message_add_book
+            name_book, author_book, year_book, genre_book = message_add_book()
+            add_book(path_file, name_book, author_book, year_book, genre_book)
+
+        elif action_in_catalog == "У":
+            from GraficUserInterface import message_delete_book, view_list_books
+            view_list_books(path_file)
+            number_book = message_delete_book()
+            delete_book(path_file, number_book)
+
+        elif action_in_catalog == "О":
+            from GraficUserInterface import view_list_books
+            print(f"В текущем каталоге находятся следующие книги:  ")
+            view_list_books(path_file)
+
+        elif action_in_catalog == "П":
+            from GraficUserInterface import message_search_books, print_list
+            search_word = message_search_books()
+            catalog = search_book(path_file, searсh_word)
+            print_list(catalog)
+
+        elif action_in_catalog == "С":
+            from GraficUserInterface import message_sort_books, print_list
+            sort_atribute = message_sort_books()
+            sort_books = sort_catalog(path_file, sort_atribute)
+            print_list(sort_books)
+
+        elif action_in_catalog == "Э":
+            from GraficUserInterface import message_export_catalog
+            format = message_export_catalog()
+            if format == "C":
+                export_catalog(path_file, name_catalog)
+                return True
+            return False
+        action_in_catalog = msg_select_act_in_catalog()
 
 
 # ФУНКЦИИ ПРИ СОЗДАНИИ КАТАЛОГА
@@ -113,6 +120,7 @@ def add_book(file, name_book, author_book, year_book, genre_book):
     file.write(name_book + author_book + year_book + genre_book)
     file.close()
 
+
 # функция удаления книги из каталога
 def delete_book(file, number_book):
     catalog = []
@@ -126,6 +134,7 @@ def delete_book(file, number_book):
             file.write(book)
         book += 1
     file.close()
+
 
 # функция поиска книги в каталоге
 def search_book(file, searсh_word):
@@ -158,9 +167,10 @@ def sort_catalog(file, sort_atribute):
             iter = 3
         words = catalog.split("- ", maxsplit=iter)
         return words[iter]
-    sort_catalog = sorted(catalog, key = get_key)
+
+    sort_catalog_ = sorted(catalog, key=get_key)
     file.close()
-    return sort_catalog
+    return sort_catalog_
 
 
 # функция экспорта каталога
@@ -168,7 +178,8 @@ def export_catalog(file, name_catalog):
     catalog = []
     file = open(file, "r")
     catalog = file.readlines()
-    df = pandas.DataFrame(catalog, columns=["Название книги","Автор книги", "Год издания", "Жанр"] )
-    df.to_csv(f"{name_catalog}.csv", index=False)
 
-
+    file_csv = open(f"{name_catalog}.csv", "w", newline="")
+    writer = csv.writer(file_csv)
+    for line in catalog:
+        writer.writerow([line])
