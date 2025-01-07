ae64c636e22b81c960cde824d7d90373ea3146e1
Ivan K <kosi17100@gmail.com>
2025-01-07 23:08:41 +0300

    (main:BLL): Переделана функция определения существования каталога в директории

--- Business_Logic_Lower.py
+++ Business_Logic_Lower.py
@@ -5,17 +5,17 @@ from Business_Logic_Upper import *
 def name_catalog():
     file = open(f"Реестр каталогов.txt", "r")
     lst_catalog = file.readlines()
-    lengt_ctalog = len(lst_catalog)
-    name_catalog = lst_catalog[lengt_ctalog - 1]
+    lengt_catalog = len(lst_catalog)
+    name_catalog = lst_catalog[lengt_catalog - 1]
     return name_catalog
 
 
 # функция проверки существования каталога перед созданием каталога
 def exist_catalog():
+    filename = name_catalog()
     path = os.getcwd()
-    for file in os.listdir(path):
-        if file.endswith(".txt") and file:
-            return True
+    if filename in os.listdir(path):
+        return True
     return False
 
 
