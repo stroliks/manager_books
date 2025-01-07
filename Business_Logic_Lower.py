3c09904d4a08450ced6b7f1e918e077c9c09f9e2
Ivan K <kosi17100@gmail.com>
2025-01-07 23:04:42 +0300

    (main:BLL): Добавлена функция определения названия последнего каталога

--- Business_Logic_Lower.py
+++ Business_Logic_Lower.py
@@ -1,6 +1,14 @@
 import os
 from Business_Logic_Upper import *
 
+# функция определение имени последнего каталога
+def name_catalog():
+    file = open(f"Реестр каталогов.txt", "r")
+    lst_catalog = file.readlines()
+    lengt_ctalog = len(lst_catalog)
+    name_catalog = lst_catalog[lengt_ctalog - 1]
+    return name_catalog
+
 
 # функция проверки существования каталога перед созданием каталога
 def exist_catalog():
